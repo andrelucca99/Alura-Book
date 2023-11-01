e15be123d2ab990b4e16c71d6e4225897459749b
Andre Lucas <andrelucca99@gmail.com>
2023-11-01 13:46:41 -0300

    feat: add banner

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,16 @@ export default function Home() {
       <header>
         <NavBar />
       </header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className='bg-blue-600 w-36 h-36 rounded-lg flex align-ite justify-center'>titulo</div>
+      <main className="w-full flex flex-col">
+        <section className='w-full h-80 bg-violet-700 text-center flex flex-col justify-center items-center gap-3'>
+          <h1 className='font-bold text-3xl text-white'>Já sabe por onde começar?</h1>
+          <p className='font-medium text-base text-white'>Encontre em nossa estante o que precisa para seu desenvolvimento!</p>
+          <input
+            className='w-80 rounded-2xl py-2 px-6 opacity-80'
+            type="text"
+            placeholder='Qual será sua próxima leitura?'
+          />
+        </section>
       </main>
     </>
   )
